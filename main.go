14b291b6a1d43e404f7715a8ec04e99575e35d74
Felyks <gamingfelix78@gmail.com>
2025-03-12 21:38:39 +0700

    tutor selesai

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -23,10 +24,41 @@ var tasks = []task{
 func main() {
 	router := gin.Default()
 	router.GET("/tasks", getTasks)
+	router.GET("/tasks/:id", getTaskById)
+	router.POST("/tasks", postTasks)
 
 	router.Run("localhost:8080")
 }
 
 func getTasks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, tasks)
+}
+
+func postTasks(c *gin.Context) {
+	var newTask task
+
+	if err := c.BindJSON(&newTask); err != nil {
+		return
+	}
+
+	tasks = append(tasks, newTask)
+	c.IndentedJSON(http.StatusCreated, newTask)
+}
+
+func getTaskById(c *gin.Context) {
+	id := c.Param("id")
+
+	taskId, err := strconv.Atoi(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "id must be integer"})
+		return
+	}
+
+	for _, t := range tasks {
+		if t.ID == taskId {
+			c.IndentedJSON(http.StatusOK, t)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "task not found"})
 }
